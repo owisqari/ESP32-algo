df7d6e76f36d6929cd15868ab4f1179e29d0f8cb
owis bukahri <owisbukhari@gmail.com>
2022-07-17 22:01:21 +0300

    update

--- TTS/script.js
+++ TTS/script.js
@@ -1,11 +1,18 @@
-texbox.addEventListener('click', function () {
+const texbox = document.getElementById('textbox')
+const startBtn= document.getElementById('start-btn');
+startBtn.addEventListener('click', function () {
     var speech = true
     window.SpeechRecognition = window.webkitSpeechRecognition
-
     const Rec = new SpeechRecognition()
-
     Rec.interimResults = true
+    Rec.addEventListener('result', e=>{
+        const script = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.script)
 
+        texbox.innerHTML = script
+    })
+    
     if(speech == true){
         Rec.start()
     }
